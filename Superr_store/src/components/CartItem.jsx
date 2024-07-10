9040646607829d292155176a48ec5f49f3a6a49a
cEbuka <kastecebuka@gmail.com>
2024-07-10 21:11:24 +0100

    worked on cart page

--- Superr_store/src/components/CartItem.jsx
+++ Superr_store/src/components/CartItem.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import propTypes from "prop-types";
 import { FaMinus, FaPlus } from "react-icons/fa";
+import { FiCheck } from "react-icons/fi";
 
 const CartItem = ({ src }) => {
+  const [isItemSelected, setIsItemSelected] = useState(false);
+  function handleSelect() {
+    setIsItemSelected((curr) => !curr);
+  }
   return (
     <React.Fragment>
       <div className="w-full h-auto flex  shadow-2xl">
         <div className="p-[10px]">
-          <div className="w-[24px] h-[24px] rounded-full border-2"></div>
+          <div
+            className={`w-[24px] h-[24px] rounded-full border-2 ${
+              isItemSelected && "bg-app-orange text-app-bg"
+            } flex items-center justify-center transition-all ease-in duration-300`}
+            onClick={handleSelect}
+          >
+            {isItemSelected && <FiCheck />}
+          </div>
         </div>
         <div className=" w-full flex gap-[16px] p-[10px] flex-2">
           <div className="w-[132px] h-[132px] bg-gray-500 flex items-center justify-center rounded-xl">
-            <img src={src} alt="product-image" className="object-cover w-[16rem] h-[8rem]" />
+            <img
+              src={src}
+              alt="product-image"
+              className="object-cover w-[16rem] h-[8rem]"
+            />
           </div>
           <div className="flex flex-col gap-[8px] p-0 flex-1">
             <div className="text-2xl">
               <p>Casio</p>
             </div>
             <span>
-              <p className="text-2xl whitespace-nowrap sm:whitespace-normal w-[150px] sm:max-w-sm text-ellipsis overflow-hidden">Casio RX 24 featuring a durable stainless steel ...</p>
+              <p className="text-2xl whitespace-nowrap sm:whitespace-normal w-[150px] sm:max-w-sm text-ellipsis overflow-hidden">
+                Casio RX 24 featuring a durable stainless steel ...
+              </p>
             </span>
             <div>
               <div>
@@ -34,11 +52,13 @@ const CartItem = ({ src }) => {
                 <p>NGN 20,000</p>
               </div>
               <div className="flex gap-[11px] ml-auto bg-slate-300 rounded-md p-1">
-                <div className="w-[25px] h-[25px] bg-app-bg rounded-md flex items-center justify-center">
+                <div className=" bg-app-bg rounded-md flex items-center justify-center">
                   <FaMinus />
                 </div>
-                <div><p className="text-xl text-app-black-1">1</p></div>
-                <div className="w-[25px] h-[25px] bg-app-bg rounded-md flex items-center justify-center">
+                <div>
+                  <p className="text-xl text-app-black-1">1</p>
+                </div>
+                <div className=" bg-app-bg rounded-md flex items-center justify-center">
                   <FaPlus />
                 </div>
               </div>
